1c113cda28e9ca0cc3cd40eccd4da87fb4e33e3d
Avi <avi@Avis-MacBook-Pro.local>
2023-09-11 20:22:39 +0300

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
-import { CustomFilter, Hero, SearchBar } from "@/components";
+import { CustomFilter, Hero, SearchBar, CarCard } from "@/components";
+import { fuels, yearsOfProduction } from "@/constants";
+import { HomeProps } from "@/types";
+import { fetchCars } from "@/utils";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home({ searchParams }: HomeProps) {
+  const allCars = await fetchCars({
+    manufacturer: searchParams.manufacturer || "",
+    year: searchParams.year || 2022,
+    fuel: searchParams.fuel || "",
+    limit: searchParams.limit || 10,
+    model: searchParams.model || "",
+  });
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
   return (
     <main className='overflow-hidden'>
       <Hero />
@@ -14,8 +25,25 @@ export default function Home() {
         <div className='home__filters'>
           <SearchBar />
           <div className='home__filter-container'>
-            <CustomFilter title='fuel' />
-            <CustomFilter title='year' />
+            <div className='home__filter-container'>
+              <CustomFilter title='fuel' options={fuels} />
+              <CustomFilter title='year' options={yearsOfProduction} />
+            </div>
+            {!isDataEmpty ? (
+              <section>
+                <div className='home__cars-wrapper'>
+                  {allCars?.map((car) => (
+                    <CarCard car={car} />
+                  ))}
+                </div>
+              </section>
+            ) : (
+              <div className='home__error-container'>
+                <h2 className='text-black text-xl font-bold'>
+                  Ooops, n cars:<p>{allCars?.message}</p>{" "}
+                </h2>
+              </div>
+            )}
           </div>
         </div>
       </div>
--- types/index.ts
+++ types/index.ts
@@ -3,11 +3,55 @@ import { MouseEventHandler } from "react";
 export interface CustomButtonProps {
   title: string;
   containerStyles?: string;
+  textStyles?: string;
   handleClick?: MouseEventHandler<HTMLButtonElement>;
   btnType?: "button" | "submit";
+  rightIcon?: any;
+  isDisabled?: boolean;
 }
 
 export interface SearchManufacturerProps {
   manufacturer: string;
   setManufacturer: (manufacturer: string) => void;
+}
+export interface FilterProps {
+  manufacturer?: string;
+  year?: number;
+  model?: string;
+  limit?: number;
+  fuel?: string;
+}
+
+export interface OptionProps {
+  title: string;
+  value: string;
+}
+
+export interface CustomFilterProps {
+  title: string;
+  options: OptionProps[];
+}
+
+export interface ShowMoreProps {
+  pageNumber: number;
+  isNext: boolean;
+}
+
+export interface HomeProps {
+  searchParams: FilterProps;
+}
+
+export interface CarProps {
+  class: string;
+  city_mpg: number;
+  combination_mpg: number;
+  cylinders: number;
+  displacement: number;
+  drive: string;
+  fuel_type: string;
+  highway_mpg: number;
+  make: string;
+  model: string;
+  transmission: string;
+  year: number;
 }
